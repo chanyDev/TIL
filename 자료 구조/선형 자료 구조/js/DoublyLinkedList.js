d87497022b783fa7f85eea7cf358b83bbf41d436
chanyDev <chanyd.dev@gmail.com>
2022-02-03 18:36:23 +0900

    create : 연결 리스트 기반 코드 작성

---
+++ 자료 구조/선형 자료 구조/js/DoublyLinkedList.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value, prev, next) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  isEmpty() {}
+
+  prepend(value) {}
+
+  append(value) {}
+
+  setHead(index) {}
+
+  access(index) {}
+
+  insert(index, value) {}
+
+  remove(index) {}
+
+  print() {}
+
+  printInv() {}
+}
